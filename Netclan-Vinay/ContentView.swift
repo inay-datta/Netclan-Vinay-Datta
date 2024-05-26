fde9e07608a0bed9afe5b784d55e71fae8cac569
Phanishree K <pk@synamedia.com>
2024-05-26 10:13:06 +0530

    Initial Commit

---
+++ Netclan-Vinay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Netclan-Vinay
+//
+//  Created by Phanishree Kommalapati on 26/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Netclan-Vinay/Netclan_VinayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netclan_VinayApp.swift
+//  Netclan-Vinay
+//
+//  Created by Phanishree Kommalapati on 26/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Netclan_VinayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
